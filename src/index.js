01ea34250357c680606c4cf08687eb0dfbd7e43c
Tatyana Atanasova <106607660+tatianna10@users.noreply.github.com>
2025-10-05 16:19:13 +0300

    Add express handlebars

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
 
 const app = express();
 
+// Setup Handlebars
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
+
+// Routes
 app.get('/', (req, res) => {
-    res.send('It works!');
+    res.render('home', { layout: false });
 });
 
+
+// Start Server
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
