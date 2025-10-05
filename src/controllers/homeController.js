9bc6c952d61afef8691f1eef90d88a47c60a9899
Tatyana Atanasova <106607660+tatianna10@users.noreply.github.com>
2025-10-05 19:35:28 +0300

    Render dynamic movies on home page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -6,10 +6,7 @@ const homeController = Router();
 homeController.get('/', (req, res) => {
     const movies = movieService.getAll();
 
-    console.log(movies);
-    
-
-    res.render('home');
+    res.render('home', { movies });
 });
 
 homeController.get('/about', (req, res) => {
