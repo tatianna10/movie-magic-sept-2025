d182ab99d0ae01e1c6b61832595cbc21d6358963
Tatyana Atanasova <106607660+tatianna10@users.noreply.github.com>
2025-10-05 20:24:45 +0300

    Add create movie page

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const movieController = Router();
 
 movieController.get('/movies/create', (req, res) => {
-    res.send('Movie Create');
+    res.render('create');
 });
 
 export default movieController;
