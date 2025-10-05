ec1421898096ed992153e361b1825a40cdc58803
Tatyana Atanasova <106607660+tatianna10@users.noreply.github.com>
2025-10-05 17:27:15 +0300

    Add about page

--- src/index.js
+++ src/index.js
@@ -19,6 +19,10 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
 
 // Start Server
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
