d7eb2f15ffc299581cbd3ee82f5e498170c4c9f6
Tatyana Atanasova <106607660+tatianna10@users.noreply.github.com>
2025-10-11 13:55:53 +0300

    Remember search words

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -24,10 +24,10 @@ movieController.get('/:movieID/details', (req, res) => {
 
 movieController.get('/search', (req, res) => {
     const filter = req.query;
-    
+
     const movies = movieService.getAll(filter);
 
-    res.render('search', { movies });
+    res.render('search', { movies, filter });
 });
 
 export default movieController;
